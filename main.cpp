f100b4d4f530b8d1553aa23b7d171ee469465a8d
Justin Alvey <jual6332@colorado.edu>
2022-03-24 05:53:43 +0000

    Added new getter/setter method for jobtitle of employee

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 class Employee {
   private:
     std::string name;
+    std::string jobTitle;
     int salary;
 
   public:
@@ -12,15 +13,52 @@ class Employee {
     salary = dollarAmount;
   }
 
+  void setJobTitle(std::string role){
+    jobTitle = role;
+  }
+
   int getSalary(){
     return salary;
   }
 
+  std::string getJobTitle(){
+    return jobTitle;
+  }
+
+};
+
+class Customer {
+  private:
+    std::string name;
+    int walletAmount;
+
+  public:
+  void setWalletAmount(int cash){
+    walletAmount = cash;
+  }
+
+  int getWalletAmoount(){
+    return walletAmount;
+  }
+
+  void makeTransaction(int purchaseAmount){
+    std::cout << "This customer has $" << walletAmount<< " dollars in his wallet."<<std::endl;
+    std::cout << "They want to make a purchase of $" << purchaseAmount << std::endl;
+    //if ( walletAmount){
+      
+    //}
+  }
 };
 
 int main() {
   Employee newEmployee;
+  newEmployee.setJobTitle("Supervisor II");
   newEmployee.setSalary(40000);
+
+  Customer newCustomer;
+  newCustomer.setWalletAmount(237);
+  newCustomer.makeTransaction(115);
+  
   std::cout << "New employee salary is: "<< newEmployee.getSalary() << std::endl;
-  std::cout << "Hello World!\n";
+  //std::cout << "Hello World!\n";
 }
